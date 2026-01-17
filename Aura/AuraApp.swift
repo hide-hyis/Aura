05fa7d7e2ccaa9f7ba734e0588d265cd7a287316
Hideyasu Ishii <ishii_pma@yahoo.co.jp>
2026-01-17 20:32:38 +0900

    Initial Commit

---
+++ Aura/AuraApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuraApp.swift
+//  Aura
+//
+//  Created by HIDEYASU ISHII on 2026/01/17.
+//
+
+import SwiftUI
+
+@main
+struct AuraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
